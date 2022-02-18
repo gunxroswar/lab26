27128596b6af91c6ce4adb9ac485f665a8584538
gunxroswar <gxz.gunforce@gmail.com>
2022-02-18 20:23:28 +0700

    "done so fast"

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -63,4 +63,18 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int loc){
+	Node *current = root;
+	Node *p = NULL;
+	if (loc == 0) root = current->next;
+	
+	else{
+		for (int i = 0; i < loc; i++){
+			if (i == loc - 1) p = current;
+				
+			current = current->next;
+		}		
+	p->next = current->next;
+	}
+	delete current;
+}
